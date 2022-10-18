1b333d6c04b70bd920d7e0960831e4d1d43071f0
unknown <atharva.shirode99@gmail.com>
2022-10-18 14:27:11 +0530

    Unused code in forms & other removed

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -12,11 +12,8 @@ const Form = () => {
     useContext(EmpContext);
   const { id } = useParams();
   const [emp, setEmp] = useState({});
-  const [empId, setEmpId] = useState("");
-
   const [buttonValue, setButtonValue] = useState("Add Employee");
   const [idPresentBefore, setIdPresentBefore] = useState(false);
-  const [message, setMessage] = useState("Id Already exists");
 
   useEffect(() => {
     const employee = empDetails.find((employee) => {
@@ -24,7 +21,6 @@ const Form = () => {
         return employee;
       }
     });
-    // console.log("30:-", id, !id);
 
     if (id != undefined && employee != undefined) {
       console.log("2.IDOFEMP", employee, !typeof employee);
@@ -35,61 +31,37 @@ const Form = () => {
 
   const formHandler = (e) => {
     e.preventDefault();
-    // if (id) {
-    //   console.log("hhh", e.target.value);
-    // }
-    // const formData = new FormData(e.target);
 
-    {
-      alert(`heloji`);
-      if (id) {
-        console.log("ID Present");
-        let leftOverEmp = [];
-        const UpdatedEmp = empDetails.filter((employee) => {
-          if (employee.empId != emp.empId) {
-            leftOverEmp.push(employee);
-            return employee;
-          }
-        });
-        leftOverEmp.push(emp);
+    if (id) {
+      console.log("ID Present");
+      let leftOverEmp = [];
+      const UpdatedEmp = empDetails.filter((employee) => {
+        if (employee.empId != emp.empId) {
+          leftOverEmp.push(employee);
+          return employee;
+        }
+      });
+      leftOverEmp.push(emp);
 
-        // setEmpDetails(leftOverEmp);
-        setEmpDetails(leftOverEmp);
-        console.log("UPDATED", leftOverEmp, "\nEMP", emp, "LEFTOVER");
-        navigate("/view-employees");
-      } else {
-        console.log("No ID", e.target.empId.value, "Q");
-        setAllEmpId(new Set([...allEmpId, e.target.empId.value]));
+      setEmpDetails(leftOverEmp);
+      console.log("UPDATED", leftOverEmp, "\nEMP", emp, "LEFTOVER");
+      navigate("/view-employees");
+    } else {
+      console.log("No ID", e.target.empId.value, "Q");
+      setAllEmpId(new Set([...allEmpId, e.target.empId.value]));
 
-        setEmpDetails([...empDetails, emp]);
-        navigate("/view-employees");
-      }
-
-      // const formData = new FormData(e.target);
-      // let empInfo = {};
-
-      // for (let [key, value] of formData.entries()) {
-      //   empInfo[key] = value;
-      //   // console.log("key-val", key, value, "Obj", empInfo1);
-      // }
-
-      // console.log(empInfo);
-      // setEmpDetails([...empDetails, empInfo]);
+      setEmpDetails([...empDetails, emp]);
+      navigate("/view-employees");
     }
-
-    // submitForm();
   };
 
   const formChanges = (e) => {
-    // console.log("FORM-Chnaged", e.target.form.Fname);
     let a = {};
-    // e.target.name;
-    // a[e.target.name] = e.target.value;
+
     if (e.target.name == "empId") {
       setIdPresentBefore(false);
       console.log("hi", [...allEmpId]);
 
-      // const seeIDUsedBefore =
       [...allEmpId].find((employeeId) => {
         console.log("148.", employeeId, e.target.value);
         if (employeeId == e.target.value) {
@@ -98,18 +70,8 @@ const Form = () => {
           return employeeId;
         }
       });
-      // console.log("SEEID", seeIDUsedBefore, seeIDUsedBefore != undefined);
-
-      // if (seeIDUsedBefore != undefined) setIdPresentBefore(true);
-      // else setIdPresentBefore(false);
-
-      // if (buttonValue != "Update Details")
-      // setEmp({ ...emp, [e.target.name]: e.target.value });
     }
-    // else {
     setEmp({ ...emp, [e.target.name]: e.target.value });
-    // }
-
     console.log("FORM-2", e.target.name, e.target.value, emp);
   };
 
